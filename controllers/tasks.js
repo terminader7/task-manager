1035ab756708bb406dc8da896d765dfe7503f944
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-03 10:33:32 -0700

    set up framework for each end point I think I'll need in controllers and routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
   res.send("all items from the file");
 };
 
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+
+const createTask = (req, res) => {
+  res.send("create task");
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
 module.exports = {
   getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
 };
