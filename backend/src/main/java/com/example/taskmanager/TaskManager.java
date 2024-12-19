984a0d54695b91aa02c45f9da928928fc90a5a6b
Nader Ebrahim <nebrahim2424@gmail.com>
2024-12-18 17:37:42 -0800

    created file structure for backend

---
+++ backend/src/main/java/com/example/taskmanager/TaskManager.java
@@ -0,0 +1,13 @@
+package com.example.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManager.class, args);
+	}
+
+}
